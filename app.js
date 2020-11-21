08f7541a72a35890be347dad620bfe643b5fb137
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:58:36 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Kilómetros a millas')
+
+alert('Kilómetros a millas')
+
+const kilometros = prompt('Ingresa la cantidad de kilómetros');
+const millas = (kilometros * 0.621371);
+
+alert(`${kilometros} kilómetros son ${millas} millas.`);
